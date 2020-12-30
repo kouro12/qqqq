ea2ddb230b335ff2434a84f197c6c9d3a4d604d3
kouro12 <2893704973@qq.com>
2020-12-30 10:14:23 +0800

    1111

---
+++ a/src/main/java/com/accp/a/Demo.java
@@ -0,0 +1,7 @@
+package com.accp.a;
+
+public class Demo {
+	public static void show() {
+		System.out.println("≤‚ ‘maven");
+	}
+}
---
+++ b/src/main/java/org/b/Run.java
@@ -0,0 +1,9 @@
+package org.b;
+
+import com.accp.a.Demo;
+
+public class Run {
+	public static void main(String[] args) {
+		Demo.show();
+	}
+}
